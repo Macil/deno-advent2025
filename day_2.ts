994a4bae13aa2c789d7f2bcaebb2a76c2625eaf9
Chris Cowan <agentme49@gmail.com>
2025-12-01 22:40:02 -0800

    day 2 part 1

---
+++ day_2.ts
@@ -0,0 +1,64 @@
+import { assertEquals } from "@std/assert";
+import { runPart } from "@macil/aocd";
+import { rangeIterator } from "@hugoalh/range-iterator";
+
+function parse(input: string): Array<[number, number]> {
+  return input.trimEnd().split(",")
+    .map((rangeStr) =>
+      rangeStr.split("-")
+        .map((s) => Number(s.trim())) as [number, number]
+    );
+}
+
+function isInvalidId(id: number): boolean {
+  const idStr = id.toString();
+  const len = idStr.length;
+  if (len % 2 === 1) return false;
+  return idStr.slice(0, len / 2) === idStr.slice(len / 2);
+}
+
+Deno.test("isInvalidId", () => {
+  assertEquals(isInvalidId(9), false);
+  assertEquals(isInvalidId(10), false);
+  assertEquals(isInvalidId(11), true);
+  assertEquals(isInvalidId(55), true);
+  assertEquals(isInvalidId(555), false);
+  assertEquals(isInvalidId(5555), true);
+  assertEquals(isInvalidId(6464), true);
+  assertEquals(isInvalidId(646), false);
+  assertEquals(isInvalidId(123123), true);
+  assertEquals(isInvalidId(123124), false);
+  assertEquals(isInvalidId(123321), false);
+});
+
+function part1(input: string): number {
+  const ranges = parse(input);
+  return Iterator.from(ranges)
+    .flatMap(([start, end]) => rangeIterator(start, end))
+    .filter(isInvalidId)
+    .reduce((sum, value) => sum + value, 0);
+}
+
+// function part2(input: string): number {
+//   const ranges = parse(input);
+//   throw new Error("TODO");
+// }
+
+if (import.meta.main) {
+  runPart(2025, 2, 1, part1);
+  // runPart(2025, 2, 2, part2);
+}
+
+const TEST_INPUT = `\
+11-22,95-115,998-1012,1188511880-1188511890,222220-222224,
+1698522-1698528,446443-446449,38593856-38593862,565653-565659,
+824824821-824824827,2121212118-2121212124
+`;
+
+Deno.test("part1", () => {
+  assertEquals(part1(TEST_INPUT), 1227775554);
+});
+
+// Deno.test("part2", () => {
+//   assertEquals(part2(TEST_INPUT), 12);
+// });
