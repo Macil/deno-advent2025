97b09dbf6d385c2ef58c2676bd6f748c36900a46
Chris Cowan <agentme49@gmail.com>
2025-12-05 03:21:13 -0800

    day 5 part 2

--- day_5.ts
+++ day_5.ts
@@ -31,14 +31,47 @@ function part1(input: string): number {
     .reduce((count) => count + 1, 0);
 }
 
-// function part2(input: string): number {
-//   const configuration = parse(input);
-//   throw new Error("TODO");
-// }
+function countOfIntegersInRange(min: number, max: number): number {
+  return max - min + 1;
+}
+
+function part2(input: string): number {
+  const configuration = parse(input);
+
+  // combine overlapping ranges
+  while (true) {
+    let mergedAny = false;
+    for (let i = 0; i < configuration.freshIngredientRanges.length - 1; i++) {
+      let rangeA = configuration.freshIngredientRanges[i];
+      for (let j = i + 1; j < configuration.freshIngredientRanges.length; j++) {
+        const rangeB = configuration.freshIngredientRanges[j];
+        if (
+          rangeA[0] <= rangeB[1] && rangeB[0] <= rangeA[1]
+        ) {
+          // ranges overlap, merge them
+          rangeA = [
+            Math.min(rangeA[0], rangeB[0]),
+            Math.max(rangeA[1], rangeB[1]),
+          ];
+          configuration.freshIngredientRanges.splice(j--, 1);
+          configuration.freshIngredientRanges[i] = rangeA;
+          mergedAny = true;
+        }
+      }
+    }
+    if (!mergedAny) {
+      break;
+    }
+  }
+
+  return Iterator.from(configuration.freshIngredientRanges)
+    .map(([min, max]) => countOfIntegersInRange(min, max))
+    .reduce((sum, value) => sum + value, 0);
+}
 
 if (import.meta.main) {
   runPart(2025, 5, 1, part1);
-  // runPart(2025, 5, 2, part2);
+  runPart(2025, 5, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -59,6 +92,6 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 3);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 14);
+});
