d5b336db75a3517f631decbe986cea9d5331e091
Chris Cowan <agentme49@gmail.com>
2025-12-12 00:53:30 -0800

    day 7 part 2

--- day_7.ts
+++ day_7.ts
@@ -2,6 +2,8 @@ import { assertEquals } from "@std/assert";
 import { runPart } from "@macil/aocd";
 import { CharacterGrid, Location } from "@macil/grid";
 import rangeIterator from "@hugoalh/range-iterator";
+import { BinaryHeap } from "@std/data-structures/binary-heap";
+import { ascend } from "@std/data-structures/comparators";
 
 function part1(input: string): number {
   const grid = CharacterGrid.fromString(input);
@@ -34,14 +36,65 @@ function part1(input: string): number {
   return activatedSplitters.size;
 }
 
-// function part2(input: string): number {
-//   const items = parse(input);
-//   throw new Error("TODO");
-// }
+function part2(input: string): number {
+  const grid = CharacterGrid.fromString(input);
+  const startPosition = grid.valuesWithLocations()
+    .find(({ value }) => value === "S")?.location;
+  if (!startPosition) {
+    throw new Error("Start position not found");
+  }
+  const activationsBySplitter = new Map<string, number>();
+  const beamQueue = BinaryHeap.from<
+    { location: Location; sourceSplitter?: Location }
+  >(
+    [{ location: startPosition, sourceSplitter: undefined }],
+    { compare: (a, b) => ascend(a.location.row, b.location.row) },
+  );
+  let beamsThatReachedBottom = 0;
+  while (true) {
+    const entry = beamQueue.pop();
+    if (!entry) {
+      break;
+    }
+    const sourceSplitterActivations = entry.sourceSplitter
+      ? activationsBySplitter.get(entry.sourceSplitter.toString())!
+      : 1;
+    const hitSplitter = rangeIterator(
+      entry.location.row,
+      grid.dimensions.rows - 1,
+    )
+      .map((row) => new Location(row, entry.location.column))
+      .find((loc) => grid.get(loc) === "^");
+    if (hitSplitter) {
+      const hitSplitterKey = hitSplitter.toString();
+
+      const splitterActivations = activationsBySplitter.get(hitSplitterKey);
+      if (splitterActivations === undefined) {
+        beamQueue.push(...[
+          {
+            location: new Location(hitSplitter.row, hitSplitter.column - 1),
+            sourceSplitter: hitSplitter,
+          },
+          {
+            location: new Location(hitSplitter.row, hitSplitter.column + 1),
+            sourceSplitter: hitSplitter,
+          },
+        ].filter((entry) => grid.isInBounds(entry.location)));
+      }
+      activationsBySplitter.set(
+        hitSplitterKey,
+        (splitterActivations ?? 0) + sourceSplitterActivations,
+      );
+    } else {
+      beamsThatReachedBottom += sourceSplitterActivations;
+    }
+  }
+  return beamsThatReachedBottom;
+}
 
 if (import.meta.main) {
   runPart(2025, 7, 1, part1);
-  // runPart(2025, 7, 2, part2);
+  runPart(2025, 7, 2, part2);
 }
 
 const TEST_INPUT = `\
@@ -67,6 +120,61 @@ Deno.test("part1", () => {
   assertEquals(part1(TEST_INPUT), 21);
 });
 
-// Deno.test("part2", () => {
-//   assertEquals(part2(TEST_INPUT), 12);
-// });
+Deno.test("part2", () => {
+  assertEquals(part2(TEST_INPUT), 40);
+});
+
+Deno.test("part2 no splitters", () => {
+  assertEquals(
+    part2(`\
+...S...
+`),
+    1,
+  );
+});
+
+Deno.test("part2 one splitter input", () => {
+  assertEquals(
+    part2(`\
+...S...
+...^...
+`),
+    2,
+  );
+});
+
+Deno.test("part2 simple input", () => {
+  assertEquals(
+    part2(`\
+...S...
+...^...
+..^.^..
+`),
+    4,
+  );
+});
+
+Deno.test("part2 overlapping paths", () => {
+  assertEquals(
+    part2(`\
+...S...
+...^...
+..^.^..
+...^...
+`),
+    6,
+  );
+});
+
+Deno.test("part2 multiple overlapping paths", () => {
+  assertEquals(
+    part2(`\
+...S...
+...^...
+..^.^..
+...^...
+..^....
+`),
+    8,
+  );
+});
